b645a6a9a9284ff5ee3ba2904195ab0b62f01104
Todor Arnaudov <twenkid@gmail.com>
2021-11-27 08:59:32 +0200

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,2 @@
+REM install XAMPP, create the DB as in the code... Good job, #27-11-2021
+cmd /v /c "set SQL_SERVER=localhost&&set SQL_UID=root&&set SQL_PWD=flask123&&set SQL_DATABASE=flask&& python app.py
