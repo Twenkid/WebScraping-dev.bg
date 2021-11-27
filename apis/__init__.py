527e6ee65c6e3f495ec5aed9e301b979eecb714b
Todor Arnaudov <twenkid@gmail.com>
2021-11-27 09:01:01 +0200

    Add files via upload

--- apis/__init__.py
+++ apis/__init__.py
@@ -4,17 +4,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_POOL_SIZE
 
+# Todor debug code: print should be a log, but that's fine for a test
 # Create the DB connection
+print("Attempt to create a DB engine...")
 engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_size=SQLALCHEMY_POOL_SIZE)
+print("Success..." + SQLALCHEMY_DATABASE_URI + ": " + str(SQLALCHEMY_POOL_SIZE))
 session = scoped_session(
     sessionmaker(autocommit=False, autoflush=True, bind=engine))
-
+print("AFT session = scoped_session(...")
 Base = declarative_base()
 Base.query = session.query_property()
-
+print("AFT Base.query")
 from . import models
 Base.metadata.create_all(bind=engine)
-
+print("AFT Base.metadata.create_all(bind=engine)")
+print("After Base.metadata.create_all(bind=engine)...")
 # Create the API
 api = Api(
     title='Web Scraping',
